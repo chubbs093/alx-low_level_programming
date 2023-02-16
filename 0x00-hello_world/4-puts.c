733710dca5434cfbb1e18f143849939c80b7f733
chubbs093 <117672664+chubbs093@users.noreply.github.com>
2023-02-16 23:07:41 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
-*This is my main function
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
