6228cac7d6493b6e25d099faf79880e16944e7ef
root <duduhamd1193@gmail.com>
2023-02-16 07:37:14 -0800

    Number 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+	
 	int i;
 	char c;
 	float f;
@@ -12,5 +13,6 @@ int main(void)
 	printf("Size of a long int: %l.\n", (unsigned long)sizeof(l));
 	printf("Size of a long long int: %l.\n", (unsigned long)sizeof(t));
 	printf("Size of a float: %l.\n", (unsigned long)sizeof(f));
+	
 	return (0);
 }
