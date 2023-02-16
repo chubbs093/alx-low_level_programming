21e6b817af7dfa3bedd6310777dcabb2df51fade
root <duduhamd1193@gmail.com>
2023-02-16 07:23:35 -0800

    Number 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char c;
+	float f;
+	long l;
+
+	printf("Size of a char: %l.\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %l.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %l.\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %l.\n", (unsigned long)sizeof(t));
+	printf("Size of a float: %l.\n", (unsigned long)sizeof(f));
+	return (0);
+}
