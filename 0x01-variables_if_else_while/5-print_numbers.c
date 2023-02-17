97d482da21f7373b094f106f9ecf4677407239ef
root <duduhamd1193@gmail.com>
2023-02-17 04:49:51 -0800

    I'm done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print all numbers in base 10 from zero.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers 0-9 using putchar.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
